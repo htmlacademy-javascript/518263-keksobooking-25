405b832074a0ff2caf80fcafc543ca73ff458e5c
Metaliy <darkspirtx@gmail.com>
2022-03-09 02:46:41 +0300

    разобрал

---
+++ js/const.js
@@ -0,0 +1,15 @@
+const dataConst = {
+  TYPE: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+
+  CHECKS_TIME: ['12:00', '13:00', '14:00'],
+
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+
+  PHOTOS: [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+  ]
+};
+
+export {dataConst};
---
+++ js/util.js
@@ -0,0 +1,31 @@
+const getRandomPositiveInteger = (a, b) => {
+
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomPositiveFloat =  (a, b, digits = 1) => {
+
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+
+  const result = Math.random() * (upper - lower) + lower;
+
+  return +result.toFixed(digits);
+};
+
+const getRandomArray = (baseArray) => {
+  const length = getRandomPositiveInteger(1, baseArray.length);
+  const newArray = new Set ([]);
+  for (let i = 0; i < length; i++) {
+    const randomElement = getRandomPositiveInteger(0, baseArray.length - 1);
+    newArray.add(baseArray[randomElement]);
+  }
+  return [...newArray];
+};
+
+export {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArray};
